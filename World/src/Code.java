7e9c1cffaa24aadee16c7e0c6d74f305856a0e4c
user <user@192.168.56.1>
2022-03-17 12:03:12 +0530

    First git commit

---
+++ World/src/Code.java
@@ -0,0 +1,14 @@
+
+public class Code {
+public static void main(String[] args)
+{
+	System.out.println("my first program");
+	System.out.println("hello java");
+	//int a =220,b=20;
+	float c = 33.9f;
+	char d = 'A';
+	System.out.println((int)c);
+	System.out.println(d);
+	System.out.println((int)d);
+}
+}
